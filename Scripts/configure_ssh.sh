7f1311aecbdce47e1b89ce54fab22c23e2034362
Hemanta Regmi <hregmi2060@gmail.com>
2025-10-21 11:03:15 +0545

    Update configure_ssh.sh

--- Scripts/configure_ssh.sh
+++ Scripts/configure_ssh.sh
@@ -8,9 +8,4 @@ touch /home/$USERNAME/.ssh/authorized_keys
 chmod 600 /home/$USERNAME/.ssh/authorized_keys
 chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
-# Harden SSH configuration
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
-systemctl restart ssh
 
-echo "SSH configured for $USERNAME with key-based login."
